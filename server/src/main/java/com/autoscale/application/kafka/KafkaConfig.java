7d0d6f3469127213668ce215277779dd94ee3649
Parth27 <diwanji.parth@gmail.com>
2021-04-26 18:05:56 -0400

    Restructure

--- server/src/main/java/com/autoscale/application/kafka/KafkaConfig.java
+++ server/src/main/java/com/autoscale/application/kafka/KafkaConfig.java
@@ -2,11 +2,11 @@ package com.autoscale.application.kafka;
 
 public class KafkaConfig {
     public static final String TOPIC_NAME = "Messages";
-    public static final String TOPIC_CREATE_SCRIPT = "/home/parth/autoscale/ec2-kafka-topic-create.sh";
-    public static final String TOPIC_UPDATE_SCRIPT = "/home/parth/autoscale/ec2-kafka-topic-update.sh";
-    public static final String REBALANCE_SCRIPT = "/home/parth/autoscale/ec2-kafka-rebalance.sh";
-    public static final String SERVER_START = "/home/parth/autoscale/ec2-kafka-start.sh";
-    public static final String SERVER_TERMINATE = "/home/parth/autoscale/ec2-kafka-stop.sh";
+    public static final String TOPIC_CREATE_SCRIPT = "/home/parth/autoscale/bin/ec2-kafka-topic-create.sh";
+    public static final String TOPIC_UPDATE_SCRIPT = "/home/parth/autoscale/bin/ec2-kafka-topic-update.sh";
+    public static final String REBALANCE_SCRIPT = "/home/parth/autoscale/bin/ec2-kafka-rebalance.sh";
+    public static final String SERVER_START = "/home/parth/autoscale/bin/ec2-kafka-start.sh";
+    public static final String SERVER_TERMINATE = "/home/parth/autoscale/bin/ec2-kafka-stop.sh";
     public static final String ZOOKEEPER_IP = "52.15.62.97";
     public static final int PRODUCER_PORT = 4567;
     public static final String PRODUCER_IP = "localhost";
--- server/src/main/java/com/autoscale/config/AutoScaleConfig.java
+++ server/src/main/java/com/autoscale/config/AutoScaleConfig.java
@@ -2,7 +2,7 @@ package com.autoscale.config;
 
 public class AutoScaleConfig {
     public static final int PORT = 1234;
-    public static final String INSTANCE_START = "/home/parth/autoscale/ec2-instance-start.sh";
+    public static final String INSTANCE_START = "/home/parth/autoscale/bin/ec2-instance-start.sh";
     public static final String MODEL_FILE = "/home/parth/autoscale/server/probabilities.txt";
     public static final int STARTUP_SERVERS = 3;
     public static final int UPPER_LIMIT = 70; // Upper limit of percent resource usage
